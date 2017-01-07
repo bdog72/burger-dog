3abf645308cd2c56f915bc2947bf9e80c77d8175
Brian Bycynski <high5bri@hotmail.com>
2017-01-07 09:31:49 -0500

    Burger Dog Day3

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
       <h2>A little bit about the restaurant</h2>
       <p>WE LOVE BURGERS AND HOT DOGS.WE LOVE BURGERS AND HOT DOGS.WE LOVE BURGERS AND HOT DOGS.WE LOVE BURGERS AND HOT DOGS.WE LOVE BURGERS AND HOT DOGS.WE LOVE BURGERS AND HOT DOGS.</p>
       <div className='fries'>
-        <img src='https://i.ytimg.com/vi/ETTyVQrUZt8/maxresdefault.jpg' height='200' />
+        <img src='https://i.ytimg.com/vi/ETTyVQrUZt8/maxresdefault.jpg' />
       </div>
     </div>
   }
